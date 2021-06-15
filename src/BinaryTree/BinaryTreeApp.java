2aad4ba1576d7f7a9a66b0a816d80a35ff2812be
LusiYustikaRachman <72186324+LusiYustikaRachman@users.noreply.github.com>
2021-06-15 17:57:48 +0700

    Update BinaryTreeApp.java

--- src/BinaryTree/BinaryTreeApp.java
+++ src/BinaryTree/BinaryTreeApp.java
@@ -1,39 +1,41 @@
 package BinaryTree;
 public class BinaryTreeApp {
-    public static void main(String[] args){
+    public static void main(String [] args) {
         BinaryTree tree = new BinaryTree();
         
         TreeNode node;
         
-        node = new TreeNode(55);
+        node = new TreeNode('D');
         tree.insert(node);
         
-        node = new TreeNode(35);
+        node = new TreeNode ('B');
         tree.insert(node);
-    
-        node = new TreeNode(25);
+        
+        node = new TreeNode ('A');
         tree.insert(node);
-    
-        node = new TreeNode(45);
+        
+        node = new TreeNode ('C');
         tree.insert(node);
-    
-        node = new TreeNode(75);
+        
+        node = new TreeNode ('F');
         tree.insert(node);
-    
-        node = new TreeNode(65);
+        
+        node = new TreeNode ('E');
         tree.insert(node);
-    
-        node = new TreeNode(85);
+        
+        node = new TreeNode ('G');
         tree.insert(node);
-    
+        
         System.out.print("Traversal dengan preorder :");
         tree.preOrder();
-        
-        System.out.print("\nTraversal dengan inorder :");
-        tree.inOrder();
-        
-        System.out.print("\nTraversal dengan postorder :");
+        System.out.println();
+        System.out.print("Traversal dengan postorder :");
         tree.postOrder();
         System.out.println();
+        System.out.print("Traversal dengan inorder :");
+        tree.inOrder();
+        System.out.println();
+        
     }
+    
 }
